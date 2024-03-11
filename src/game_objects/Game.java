190b38be844698632b4daa26d2195b7ccf6b18e9
EvanGibsonSmith <114107230+EvanGibsonSmith@users.noreply.github.com>
2024-03-10 20:52:25 -0400

    Simple game made
    The game object is very simple and just runs a bunch of full rounds until there is a winner. A TestGame was created that can be run in the terminal to see that everything is working properly.These are not passing or failing tests though, just examined by playing the game at the moment.

---
+++ src/game_objects/Game.java
@@ -0,0 +1,43 @@
+package src.game_objects;
+
+import java.util.Stack;
+
+public class Game {
+    int startingPlayerCursor;
+    Player[] players;
+    int winnerCursor = -1;
+    Stack<FullRound> rounds = new Stack<>(); // NOTE: we might not technically need to track rounds, but they are nice to have
+
+    public int getWinnerCursor() {
+        for (int i=0; i<players.length; ++i) {
+            Player p = players[i];
+            if (p.getPoints()>=2) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean isWinner() {
+        for (Player p: players) {
+            if (p.getPoints()>=2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Game(Player[] players, int startingPlayerCursor) {
+        this.players = players;
+        this.startingPlayerCursor = startingPlayerCursor;
+    }
+
+    public void runGame() {
+        while (!isWinner()) {
+            FullRound newRound = new FullRound(players, startingPlayerCursor);
+            newRound.runRound();
+            rounds.add(newRound); // stores information for this round if needed
+        }
+        winnerCursor = getWinnerCursor();
+    }
+}
---
+++ test/TestGame.java
@@ -0,0 +1,24 @@
+package test;
+
+import src.game_objects.Game;
+import src.game_objects.Player;
+
+public class TestGame {
+    
+    public static void main(String[] args) {
+        Player[] players = new Player[3];
+        for (int i=0; i<players.length; ++i) {
+            players[i] = new Player();
+        }
+        Game game = new Game(players, 0);
+        game.runGame();
+        
+        // print player information
+        for (int i=0; i<players.length; ++i) { // not showing played cards since they are returned to hand
+            Player p = players[i]; 
+            System.out.println("Player " + i);
+            System.out.println("\t Hand: " + p.getHand());
+            System.out.println("\t Points " + p.getPoints());
+        }
+    }
+}
